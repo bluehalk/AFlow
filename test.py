c5e80dee527e880bf98e2c18bb65b2a0d45bca02
codiplay <codiplay@bcm.cm.cluster>
2025-06-29 23:08:39 +0800

    0

---
+++ test.py
@@ -0,0 +1,19 @@
+import os
+from groq import Groq
+
+client = Groq(
+    api_key="",
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Explain the importance of fast language models",
+        }
+    ],
+    model="llama-3.3-70b-versatile",
+    stream=False,
+)
+
+print(chat_completion.choices[0].message.content)
